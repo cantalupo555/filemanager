016721f61234f74994f99758765acf14678c224d
cantalupo555 <cantalupo@protonmail.com>
2018-08-04 08:29:19 -0300

    Update

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * Installer for File Manager Extplorer Module for Sentora 1.0.0
+ * Version : 100
+ * Author : cantalupo555 - cantalupo@protonmail.com
+ * Contribution : TGates
+ * Suggestion : Me.B
+ *
+ */
+echo "<meta HTTP-EQUIV='Refresh' CONTENT='2;URL=/modules/filemanager/install.php'>";
+?>
